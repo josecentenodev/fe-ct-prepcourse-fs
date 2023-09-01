d0f5edc09a2f4900ba8375eebe5a6e0b196c4493
Centeno Jose Antonio <jocecito182@gmail.com>
2023-09-01 19:02:33 -0300

    Add M08 Resolution

--- M07 JavaScript Clases/Ejercicio 01.js
+++ M07 JavaScript Clases/Ejercicio 01.js
@@ -2,31 +2,58 @@
 /*1️⃣ EJERCICIO 01 1️⃣*/
 
 function crearUsuario() {
-   // Crea una Clase de ES6 o una función constructora llamada "Usuario".
-   // El constructor de esta clase debe tener las propiedades: "usuario", "nombre", "email" y "password".
-   // El valor de cada propiedad la recibirá por parámetro.
-   // Además, esta clase debe tener un método llamado "saludar".
-   // Este método debe retornar el string: 'Hola, mi nombre es {{nombre}}'.
-   // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
-   // Retornar la clase.
-   // Tu código:
+  // Crea una Clase de ES6 o una función constructora llamada "Usuario".
+  // El constructor de esta clase debe tener las propiedades: "usuario", "nombre", "email" y "password".
+  // El valor de cada propiedad la recibirá por parámetro.
+  // Además, esta clase debe tener un método llamado "saludar".
+  // Este método debe retornar el string: 'Hola, mi nombre es {{nombre}}'.
+  // El valor {{nombre}} debe ser el nombre definido para la instancia de la clase.
+  // Retornar la clase.
+  // Tu código:
+  return class Usuario {
+    constructor(usuario, nombre, email, password) {
+      this.usuario = usuario;
+      this.nombre = nombre;
+      this.email = email;
+      this.password = password;
+    }
+
+    saludar() {
+      return `Hola, mi nombre es ${this.nombre}`;
+    }
+  };
 }
 
 function agregarMetodoPrototype(Usuario) {
-   // Agrega un método al prototipo de "Usuario".
-   // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
-   // Tu código:
+  // Agrega un método al prototipo de "Usuario".
+  // El método debe llamarse "saludar" y debe devolver el string "Hello World!".
+  // Tu código:
+  Usuario.prototype.saludar = function () {
+    return "Hello World!";
+  };
+
+  return Usuario;
 }
 
 function agregarStringInvertida() {
-   // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
-   // El método debe llamarse "reverse".
-   // [PISTA]: necesitarás utilizar el objeto "this".
+  // Agrega un método al prototipo de "String" que devuelva la misma cadena de caracteres, pero invertida.
+  // El método debe llamarse "reverse".
+  // [PISTA]: necesitarás utilizar el objeto "this".
+  String.prototype.reverse = function () {
+    let arrayFromString = [];
+    let fromArrayToString;
+    const fromStringToArray = this.split("");
+    for (var i = 0; i < fromStringToArray.length; i++) {
+      arrayFromString.unshift(fromStringToArray[i]);
+    }
+    fromArrayToString = arrayFromString.join("");
+    return fromArrayToString;
+  };
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   crearUsuario,
-   agregarMetodoPrototype,
-   agregarStringInvertida,
+  crearUsuario,
+  agregarMetodoPrototype,
+  agregarStringInvertida,
 };
--- M08 JavaScript Callback/homework.js
+++ M08 JavaScript Callback/homework.js
@@ -1,60 +1,78 @@
 /*⚠️ NO MODIFIQUES EL NOMBRE DE LAS DECLARACIONES ⚠️*/
 
 function mayuscula(nombre) {
-   // Esta función recibe un nombre (string).
-   // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
-   // [Ejemplo]: "mario" ----> "Mario".
-   // Tu código:
+  // Esta función recibe un nombre (string).
+  // Debe devolver el mismo nombre, pero con la primera letra en mayúscula.
+  // [Ejemplo]: "mario" ----> "Mario".
+  // Tu código:
+  const primeraLetra = nombre[0];
+  const restoDeNombre = nombre.slice(1);
+
+  return primeraLetra.toUpperCase() + restoDeNombre;
 }
 
 function invocarCallback(cb) {
-   // Invoca/ejecuta el callback `cb`.
-   // [NOTA]: no debes reotrnar nada.
-   // Tu código:
+  // Invoca/ejecuta el callback `cb`.
+  // [NOTA]: no debes reotrnar nada.
+  // Tu código:
+  cb();
 }
 
 function operacionMatematica(num1, num2, cb) {
-   // En este ejercicio recibirás dos números y un callback.
-   // El callback realiza una operación matemática, por lo que necesita de los dos números.
-   // Retorna el resultado del callback pasándole como parámetros los números.
-   // Tu código:
+  // En este ejercicio recibirás dos números y un callback.
+  // El callback realiza una operación matemática, por lo que necesita de los dos números.
+  // Retorna el resultado del callback pasándole como parámetros los números.
+  // Tu código:
+  return cb(num1, num2);
 }
 
 function sumarArray(arrayOfNumbers, cb) {
-   // Recibes un arreglo de números y un callback.
-   // Suma todos los números del arreglo.
-   // Este resultado debes pasárselo como argumento al callback recibido.
-   // [NOTA]: no debes reotrnar nada.
-   // Tu código:
+  // Recibes un arreglo de números y un callback.
+  // Suma todos los números del arreglo.
+  // Este resultado debes pasárselo como argumento al callback recibido.
+  // [NOTA]: no debes reotrnar nada.
+  // Tu código:
+  let suma = 0;
+  for (let index = 0; index < arrayOfNumbers.length; index++) {
+    suma = suma + arrayOfNumbers[index];
+  }
+  cb(suma);
 }
 
 function forEach(array, cb) {
-   // Recibes un arreglo y un callback.
-   // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
-   // Debes pasarle el elemento como argumento al callback.
-   // Tu código:
+  // Recibes un arreglo y un callback.
+  // Debes iterar sobre el arreglo, y por cada elemento ejecutar el callback.
+  // Debes pasarle el elemento como argumento al callback.
+  // Tu código:
+  for (let index = 0; index < array.length; index++) {
+    cb(array[index]);
+  }
 }
 
 function map(array, cb) {
-   // Debes iterar sobre el arreglo, y cada elemento pasárselo como arguemento al callback.
-   // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
-   // Retorna el nuevo arreglo.
-   // Tu código:
+  // Debes iterar sobre el arreglo, y cada elemento pasárselo como arguemento al callback.
+  // Tiene que guardar el resultado devuelto por el callback en cada elemento dentro de un nuevo arreglo.
+  // Retorna el nuevo arreglo.
+  // Tu código:
+  let arregloCB = [];
+  array.forEach((item) => arregloCB.push(cb(item)));
+  return arregloCB;
 }
 
 function filter(arrayOfStrings) {
-   // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
-   // Luego retorna un nuevo arreglo con estos elementos.
-   // Tu código:
+  // Debes identificar todos los elementos el arreglo que comiencen con la letra "a".
+  // Luego retorna un nuevo arreglo con estos elementos.
+  // Tu código:
+  return arrayOfStrings.filter((item) => item[0] === "a");
 }
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
-   mayuscula,
-   invocarCallback,
-   operacionMatematica,
-   sumarArray,
-   forEach,
-   map,
-   filter,
+  mayuscula,
+  invocarCallback,
+  operacionMatematica,
+  sumarArray,
+  forEach,
+  map,
+  filter,
 };
